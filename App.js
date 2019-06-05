99567f5897fba3ece96a2735f8d9f5cdc80fc7f0
Bygones <bygones@isshw.cn>
2019-06-05 19:45:09 +0800

    提交

---
+++ App.js
@@ -0,0 +1,45 @@
+/**
+ * 淘宝领券王
+ */
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, WebView, Platform, BackHandler, ToastAndroid } from 'react-native';
+
+
+export default class App extends Component<Props> {
+    onNavigationStateChange = navState => {
+        this.setState({
+            backButtonEnabled: navState.canGoBack
+        });
+    };
+    componentDidMount() {
+        if (Platform.OS === 'android') {
+            BackHandler.addEventListener('hardwareBackPress', this.onBackAndroid);
+        }
+    }
+    onBackAndroid = () => {
+        if (this.state.backButtonEnabled) {
+            this.refs['webView'].goBack();
+            return true;
+        } else {
+            if (this.lastBackPressed && this.lastBackPressed + 2000 >= Date.now()) {
+                return false;
+            }
+            this.lastBackPressed = Date.now();
+            ToastAndroid.show('再按一次退出应用', ToastAndroid.SHORT);
+            return true;
+        }
+    };
+    render() {
+        return (
+            <WebView
+                source={{ uri: 'http://abc.isshw.cn/' }}
+                style={{ width: '100%', height: '100%' }}
+                ref="webView"
+                onNavigationStateChange={this.onNavigationStateChange}
+            />
+        );
+    }
+}
+const styles = StyleSheet.create({
+
+});
---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () =>{
+    SplashScreen.hide();
+    return App;
+});
+console.disableYellowBox = true;
